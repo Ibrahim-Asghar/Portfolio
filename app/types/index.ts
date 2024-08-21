ca340b383a42db5ea7453278fb05f60faa69356a
MUHAMMAD IBRAHIM <132449994+Ibrahim-Asghar@users.noreply.github.com>
2024-08-21 18:57:07 +0500

    Update index.ts

--- app/types/index.ts
+++ app/types/index.ts
@@ -14,6 +14,7 @@ export type ProfileType = {
   email: string,
   fullBio: PortableTextBlock[],
   location: string,
+  imageURL?: string; 
   resumeURL: string,
   socialLinks: string[],
   skills: string[],
